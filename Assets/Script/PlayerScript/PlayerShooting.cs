a11f9e46eb7e53fb2090409ec49eb618dbb5d0b6
ChoiSenn <quitechoisen@gmail.com>
2022-01-31 22:23:26 +0900

    Audio

--- Assets/Script/PlayerScript/PlayerShooting.cs
+++ Assets/Script/PlayerScript/PlayerShooting.cs
@@ -8,6 +8,12 @@ public class PlayerShooting : MonoBehaviour
     private GameObject projectilePrefab;  // 공격할 때 생성되는 발사체 프리팹
     [SerializeField]
     private float attackRate = 0.1f;  // 공격 속도
+    private AudioSource audioSource;  // 사운드 재생 컴포넌트
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void StartFiring()
     {
@@ -24,6 +30,7 @@ public class PlayerShooting : MonoBehaviour
         while (true)
         {
             Instantiate(projectilePrefab, transform.position, Quaternion.identity);  // 발사체 오브젝트 생성
+            audioSource.Play();  // 호출되었을 때 사운드 재생
 
             yield return new WaitForSeconds(attackRate);  // attackRate 시간 만큼 대기
         }
